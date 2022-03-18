66066fd156ee6a76ebc3c77a0380573da19cd798
rebelattogustavo <gu.zapella@gmail.com>
2022-03-18 14:33:12 -0300

    update final

--- src/Main.java
+++ src/Main.java
@@ -2,23 +2,27 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
+
     static Scanner tec = new Scanner(System.in);
 
     public static void main(String[] args) {
         ArrayList<Carro> listaCarros = new ArrayList<Carro>();
+        ArrayList<Carro> listaCarrosVendidos = new ArrayList<Carro>();
         ArrayList<Moto> listaMotos = new ArrayList<Moto>();
+        ArrayList<Moto> listaMotosVendidas = new ArrayList<Moto>();
 
-        menuPrincipal(listaCarros, listaMotos);
+        menuPrincipal(listaCarros, listaMotos,listaCarrosVendidos,listaMotosVendidas);
     }
 
 
-    public static void menuPrincipal(ArrayList<Carro> listaCarros, ArrayList<Moto> listaMotos){
+    public static void menuPrincipal(ArrayList<Carro> listaCarros, ArrayList<Moto> listaMotos, ArrayList<Carro> listaCarrosVendidos, ArrayList<Moto> listaMotosVendidas){
         System.out.println("--- MENU PRINCIPAL ---" +
                 "\n1- Cadastrar" +
                 "\n2- Listar" +
                 "\n3- Editar" +
                 "\n4- Remover" +
-                "\n5- Encerrar");
+                "\n5- Vender" +
+                "\n6- Encerrar");
         int opcao = tec.nextInt();
 
         switch (opcao){
@@ -27,43 +31,67 @@ public class Main {
                 switch(opcaoTipo){
                     case 1:
                         listaCarros.add(cadastrarCarro());
-                        menuPrincipal(listaCarros, listaMotos);
+                        menuPrincipal(listaCarros, listaMotos,listaCarrosVendidos,listaMotosVendidas);
                         break;
                     case 2:
                         listaMotos.add(cadastrarMoto());
-                        menuPrincipal(listaCarros, listaMotos);
+                        menuPrincipal(listaCarros, listaMotos,listaCarrosVendidos,listaMotosVendidas);
                         break;
                     case 3:
-                        menuPrincipal(listaCarros,listaMotos);
+                        menuPrincipal(listaCarros,listaMotos,listaCarrosVendidos,listaMotosVendidas);
                         break;
                 }
                 break;
             case 2:
                 opcaoTipo = selecionarTipo("Listar: ");
+                if (opcaoTipo == 3){
+                    menuPrincipal(listaCarros,listaMotos,listaCarrosVendidos,listaMotosVendidas);
+                }
+                int listar = selecionarTipoListar("Informe o que deseja ver: ");
                 switch (opcaoTipo){
                     case 1:
-                        for (Carro carro : listaCarros){
-                            System.out.println(carro.toString());
+                        switch (listar){
+                            case 1:
+                                for (Carro carro : listaCarros){
+                                    System.out.println(carro.toString());
+                                }
+                                menuPrincipal(listaCarros, listaMotos,listaCarrosVendidos,listaMotosVendidas);
+                                break;
+                            case 2:
+                                for (Carro carro : listaCarrosVendidos){
+                                    System.out.println(carro.toString());
+                                }
+                                menuPrincipal(listaCarros, listaMotos,listaCarrosVendidos,listaMotosVendidas);
+                                break;
                         }
-                        menuPrincipal(listaCarros, listaMotos);
                         break;
                     case 2:
-                        for (Moto moto : listaMotos){
-                            System.out.println(moto.toString());
+                        switch (listar){
+                            case 1:
+                                for (Moto moto : listaMotos){
+                                    System.out.println(moto.toString());
+                                }
+                                menuPrincipal(listaCarros, listaMotos,listaCarrosVendidos,listaMotosVendidas);
+                                break;
+                            case 2:
+                                for (Moto moto : listaMotosVendidas){
+                                    System.out.println(moto.toString());
+                                }
+                                menuPrincipal(listaCarros, listaMotos,listaCarrosVendidos,listaMotosVendidas);
+                                break;
                         }
-                        menuPrincipal(listaCarros, listaMotos);
-                        break;
-                    case 3:
-                        menuPrincipal(listaCarros,listaMotos);
                         break;
                 }
                 break;
             case 3:
                 opcaoTipo = selecionarTipo("Editar");
-                int indice = verificaAutomovel(listaCarros,listaMotos,opcaoTipo);
-                if(verificaAutomovel(listaCarros,listaMotos,opcaoTipo) == -1){
+                if(opcaoTipo == 3){
+                    menuPrincipal(listaCarros,listaMotos,listaCarrosVendidos,listaMotosVendidas);
+                }
+                int indice = verificaAutomovel(listaCarros,listaMotos,listaCarrosVendidos,listaMotosVendidas,opcaoTipo);
+                if(verificaAutomovel(listaCarros,listaMotos,listaCarrosVendidos,listaMotosVendidas,opcaoTipo) == -1){
                     System.out.println("Placa não encontrada!");
-                    menuPrincipal(listaCarros,listaMotos);
+                    menuPrincipal(listaCarros,listaMotos,listaCarrosVendidos,listaMotosVendidas);
                 }
                 int editar = selecionarEditar("Informe o que deseja editar: ");
                 switch (opcaoTipo){
@@ -71,11 +99,11 @@ public class Main {
                         switch (editar){
                             case 1:
                                 listaCarros.set(indice,cadastrarCarro());
-                                menuPrincipal(listaCarros,listaMotos);
+                                menuPrincipal(listaCarros,listaMotos,listaCarrosVendidos,listaMotosVendidas);
                                 break;
                             case 2:
                                 editarAtributo(indice, listaCarros, listaMotos, opcaoTipo);
-                                menuPrincipal(listaCarros,listaMotos);
+                                menuPrincipal(listaCarros,listaMotos,listaCarrosVendidos,listaMotosVendidas);
                                 break;
                         }
                         break;
@@ -83,36 +111,48 @@ public class Main {
                         switch (editar){
                             case 1:
                                 listaMotos.set(indice, cadastrarMoto());
-                                menuPrincipal(listaCarros,listaMotos);
+                                menuPrincipal(listaCarros,listaMotos,listaCarrosVendidos,listaMotosVendidas);
                                 break;
                             case 2:
                                 editarAtributo(indice,listaCarros,listaMotos,opcaoTipo);
-                                menuPrincipal(listaCarros,listaMotos);
+                                menuPrincipal(listaCarros,listaMotos,listaCarrosVendidos,listaMotosVendidas);
                                 break;
                         }
                         break;
-                    case 3:
-                        menuPrincipal(listaCarros,listaMotos);
-                        break;
                 }
-                menuPrincipal(listaCarros,listaMotos);
                 break;
             case 4:
                 opcaoTipo = selecionarTipo("Remover: ");
-                indice = verificaAutomovel(listaCarros,listaMotos,opcaoTipo);
-                if(verificaAutomovel(listaCarros,listaMotos,opcaoTipo) == -1){
-                    System.out.println("Placa não encontrada!");
-                    menuPrincipal(listaCarros,listaMotos);
-                }else{
-                excluirCarro(listaCarros,listaMotos,opcaoTipo,indice);
-                menuPrincipal(listaCarros,listaMotos);
+                if (opcaoTipo == 3){
+                    menuPrincipal(listaCarros,listaMotos,listaCarrosVendidos,listaMotosVendidas);
                 }
+                indice = verificaAutomovel(listaCarros,listaMotos,listaCarrosVendidos,listaMotosVendidas,opcaoTipo);
+                if(verificaAutomovel(listaCarros,listaMotos,listaCarrosVendidos,listaMotosVendidas,opcaoTipo) == -1){
+                    System.out.println("Placa não encontrada!");
+                    menuPrincipal(listaCarros,listaMotos,listaCarrosVendidos,listaMotosVendidas);
+                }else{
+                excluirCarro(listaCarros,listaMotos,listaCarrosVendidos,listaMotosVendidas,opcaoTipo,indice);
+                }
+                menuPrincipal(listaCarros,listaMotos,listaCarrosVendidos,listaMotosVendidas);
                 break;
             case 5:
+                opcaoTipo = selecionarTipo("Vender: ");
+                if (opcaoTipo == 3){
+                    menuPrincipal(listaCarros,listaMotos,listaCarrosVendidos,listaMotosVendidas);
+                }
+                indice = verificaAutomovel(listaCarros,listaMotos,listaCarrosVendidos,listaMotosVendidas,opcaoTipo);
+                if(verificaAutomovel(listaCarros,listaMotos,listaCarrosVendidos,listaMotosVendidas,opcaoTipo) == -1){
+                    System.out.println("Placa não encontrada!");
+                    menuPrincipal(listaCarros,listaMotos,listaCarrosVendidos,listaMotosVendidas);
+                }else{
+                    venderAutomovel(listaCarros,listaCarrosVendidos,listaMotos,listaMotosVendidas,opcaoTipo,indice);
+                }
+                menuPrincipal(listaCarros,listaMotos,listaCarrosVendidos,listaMotosVendidas);
+                break;
+            case 6:
                 System.exit(0);
                 break;
         }
-
     }
 
     private static Automovel cadastrarAutomovel(){
@@ -155,10 +195,9 @@ public class Main {
         Moto moto = new Moto(automovel.getMarca(), automovel.getModelo(), automovel.getNumPlaca(), cilindradas,
                 alternador, taxaCompressao);
         return  moto;
-
     }
 
-    private static int verificaAutomovel(ArrayList<Carro> listaCarros, ArrayList<Moto> listaMotos, int opcaoTipo){
+    private static int verificaAutomovel(ArrayList<Carro> listaCarros, ArrayList<Moto> listaMotos, ArrayList<Carro> listaCarrosVendidos, ArrayList<Moto> listaMotosVendidas, int opcaoTipo){
         System.out.println("Informe a placa do veículo: ");
         String numPlaca = tec.next();
         switch (opcaoTipo){
@@ -181,7 +220,7 @@ public class Main {
                 }
                 break;
             case 3:
-                menuPrincipal(listaCarros,listaMotos);
+                menuPrincipal(listaCarros,listaMotos,listaCarrosVendidos,listaMotosVendidas);
                 break;
         }
         return -1;
@@ -194,14 +233,18 @@ public class Main {
         return tec.nextInt();
     }
 
+    private static void editaAutomovel(ArrayList<Carro> listaCarros,ArrayList<Moto> listaMotos){
+        System.out.println("Informe o que deseja editar:" +
+                "\n1- Marca" +
+                "\n2- Modelo" +
+                "\n3- Número da placa");
+    }
+
     private static void editarAtributo(int indice,ArrayList<Carro> listaCarros,ArrayList<Moto> listaMotos,int opcaoTipo){
         switch (opcaoTipo){
             case 1:
-                System.out.println("Informe o que deseja editar: " +
-                        "\n1- Marca" +
-                        "\n2- Modelo" +
-                        "\n3- Número da placa" +
-                        "\n4- Quantidade de portas" +
+                editaAutomovel(listaCarros,listaMotos);
+                System.out.println("4- Quantidade de portas" +
                         "\n5- Cavalos" +
                         "\n6- Tração");
                 int opcao = tec.nextInt();
@@ -233,11 +276,7 @@ public class Main {
                 }
                 break;
             case 2:
-                System.out.println("Informe o que deseja editar: " +
-                        "\n1- Marca" +
-                        "\n2- Modelo" +
-                        "\n3- Número da placa" +
-                        "\n4- Cilindradas" +
+                System.out.println("4- Cilindradas" +
                         "\n5- Alternador" +
                         "\n6- Taxa de compressão");
                 opcao = tec.nextInt();
@@ -271,7 +310,7 @@ public class Main {
         }
     }
 
-    private static void excluirCarro(ArrayList<Carro> listaCarros,ArrayList<Moto> listaMotos,int opcaoTipo,int indice){
+    private static void excluirCarro(ArrayList<Carro> listaCarros,ArrayList<Moto> listaMotos,ArrayList<Carro> listaCarrosVendidos,ArrayList<Moto> listaMotosVendidas,int opcaoTipo,int indice){
         System.out.println("Realmente deseja excluir esse veículo?");
         char resposta = tec.next().charAt(0);
         if(resposta == 's' || resposta == 'S'){
@@ -283,13 +322,35 @@ public class Main {
                     listaMotos.remove(indice);
                     break;
                 case 3:
-                    menuPrincipal(listaCarros,listaMotos);
+                    
                     break;
             }
         }else{
-            menuPrincipal(listaCarros,listaMotos);
+            menuPrincipal(listaCarros,listaMotos,listaCarrosVendidos,listaMotosVendidas);
         }
 
+    }
+
+    private static int selecionarTipoListar(String opcaoMenu){
+        System.out.println(opcaoMenu +
+                "\n1- Em estoque" +
+                "\n2- Vendidos");
+        return tec.nextInt();
+    }
+
+    private static void venderAutomovel(ArrayList<Carro> listaCarros,ArrayList<Carro> listaCarrosVendidos,ArrayList<Moto> listaMotos,ArrayList<Moto> listaMotosVendidas,int opcaoTipo,int indice){
+        switch (opcaoTipo){
+            case 1:
+                listaCarrosVendidos.add(listaCarros.get(indice));
+                listaCarros.remove(indice);
+                System.out.println("Carro vendido com sucesso!");
+                break;
+            case 2:
+                listaMotosVendidas.add(listaMotos.get(indice));
+                listaMotos.remove(indice);
+                System.out.println("Moto vendida com sucesso!");
+                break;
+        }
     }
 
     private static int selecionarTipo(String opcaoMenu){
